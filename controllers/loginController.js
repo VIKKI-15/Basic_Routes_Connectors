47a2e8dd25bc3fc361ebfa7361973f159c2c7941
Vicky <vickykumawat9593@gmail.com>
2023-10-02 11:33:28 +0530

    coments

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -3,6 +3,9 @@ const path = require("path");
 const app = express();
 
 module.exports = (req, res) => {
+  // ANOTHER WAY TO PROVIDE DIRECTORY
+  // const filePath = path.join(__dirname, "../public/login.html");
+
   const filePath = path.join(__dirname, "..", "public", "login.html");
   // the ".." (double dot) means go back two time in directory
   // ways to provide file path
